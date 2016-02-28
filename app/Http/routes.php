815cc63632dc5c0b31861f4686a08991f20ff9c8
Lucas Dimas Cavalcante Lopes <lucasdcavalcante@gmail.com>
2016-02-27 21:05:19 -0300

    Teste simples de rotas e view

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('produtos','TestController@index');
 /*
 |--------------------------------------------------------------------------
 | Application Routes
