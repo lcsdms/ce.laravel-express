f7ddee45d9ca7e07ad306149075e055134f30ffe
Lucas Dimas Cavalcante Lopes <lucasdcavalcante@gmail.com>
2016-05-14 16:35:03 -0300

    Adicionado models e tabelas de relacionamento entre tags e posts
    Configurado tambem a tabela auxiliar entre tags e posts

--- database/migrations/2016_05_14_184137_create_tags_posts_table.php
+++ database/migrations/2016_05_14_184137_create_tags_posts_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCommentsTable extends Migration
+class CreateTagsPostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,16 +12,11 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
-            $table->increments('id');
-            //Criação da FK para linkar com a tabela de posts
-            //Link do campo post_id com o id da tabela posts
+        Schema::create('posts_tags', function (Blueprint $table) {
             $table->integer('post_id')->unsigned();
             $table->foreign('post_id')->references('id')->on('posts');
-            $table->text('comment');
-            $table->string('name');
-            $table->string('email');
-            $table->timestamps();
+            $table->integer('tag_id')->unsigned();;
+            $table->foreign('tag_id')->references('id')->on('tags');
         });
     }
 
@@ -32,6 +27,6 @@ class CreateCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('comments');
+        Schema::drop('posts_tags');
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,5 +6,10 @@
         <h2>{{$post->title}}</h2>
         <p>{{$post->content}}</p>
         <hr>
+        <h3>Comments</h3>
+        @foreach($post->comments as $comment)
+            <b>Nome: {{ $comment->name }}</b>
+            <b>Comment: {{ $comment->$comment }}</b>
+        @endforeach
     @endforeach
 @endsection
