1bda1441e8b6d37c1431c248f721e0fcb8a4d377
Lucas Dimas Cavalcante Lopes <lucasdcavalcante@gmail.com>
2016-05-21 13:21:04 -0300

    Criação da pagina listando os posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,11 +5,18 @@
     @foreach($posts as $post)
         <h2>{{$post->title}}</h2>
         <p>{{$post->content}}</p>
-        <hr>
+        <b>Tags:</b>
+        <ul>
+            @foreach($post->tags as $tag)
+                 <li>{{ $tag->name }}</li>
+            @endforeach
+        </ul>
         <h3>Comments</h3>
         @foreach($post->comments as $comment)
-            <b>Nome: {{ $comment->name }}</b>
-            <b>Comment: {{ $comment->$comment }}</b>
+            <b>Nome: {{ $comment->name }}</b> <br>
+            <b>Comment: {{ $comment->comment }}</b>
         @endforeach
+        <hr>
     @endforeach
+
 @endsection
