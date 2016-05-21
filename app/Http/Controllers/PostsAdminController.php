db1887c06c27f7bd9d4299819155f9d317eef1df
Lucas Dimas Cavalcante Lopes <lucasdcavalcante@gmail.com>
2016-05-21 16:52:15 -0300

    Criação de rotas novas e salvando os dados no banco

--- app/Http/Controllers/PostsAdminController.php
+++ app/Http/Controllers/PostsAdminController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Post;
+
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
 
 class PostsAdminController extends Controller
 {
@@ -15,9 +15,27 @@ class PostsAdminController extends Controller
     public function __construct(Post $post){
         $this->post = $post;
     }
+
+    //Rota de redirecionamento para a pagina de listagem dos posts, com paginação
     public function index(){
-        
         $posts = $this->post->paginate(10);
         return view('admin.posts.index',compact('posts'));
+    }
+
+    //Rota de redirecionamento para a página de Criação de um novo Post
+    public function create(){
+        return view('admin.posts.create');
+    }
+
+    //Função do controller para gravação dos dados, que vai receber um request POST
+    public function store(Request $request){
+
+        //dd($request->all()); //Função para executar o dump do request e mostrar na tela, matando a aplicação
+
+        //Cria um novo post com base no request enviado
+        $this->post->create($request->all());
+
+        //Apos a criação, redireciona para a página inicial
+        return redirect()->route('admin.posts.index');
     }
 }
