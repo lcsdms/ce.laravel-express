06089250ae12088959b450eb0fd736fbf0074099
Lucas Dimas Cavalcante Lopes <lucasdcavalcante@gmail.com>
2016-05-21 14:38:15 -0300

    Implementação da listagem dos posts

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -6,6 +6,19 @@
 @section('content')
     <h1>Blog Admin</h1>
 
-
+    <table class="table">
+        <tr>
+            <th>ID</th>
+            <th>TITLE</th>
+            <th>ACTION</th>
+        </tr>
+        @foreach($posts as $post)
+            <tr>
+                <td>{{ $post->id }}</td>
+                <td>{{ $post->title }}</td>
+                <td> </td>
+            </tr>
+        @endforeach
+    </table>
 @endsection
 
